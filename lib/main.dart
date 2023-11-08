fcc7624cb1f1f69f19ea4c87a77107c5b0c19692
SHINDONGDONG <homerjay@naver.com>
2023-11-08 10:32:24 +0900

    splash yaml file, add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_app/utils/theme/theme.dart';
 
 void main() {
   runApp(const App());
@@ -6,13 +7,18 @@ void main() {
 
 class App extends StatelessWidget {
   const App({super.key});
+  //TODO: add Widgets Binding
+  //TODO: Init Local Storage
+  //TODO: Await Native Slash
+  //TODO: Init Firebase
+  //TODO: Init Auth
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.system,
-      theme: ThemeData(textTheme: TextTheme()),
-      darkTheme: ThemeData(),
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
     );
   }
 }
