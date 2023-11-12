d95c4cb36f77eb737d5b32b280bf0121c451a286
신민철 <homerjay@naver.com>
2023-11-12 14:59:18 +0900

    ADD SCREEN : Home Appbar, icon ...

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_app/common/widgets/custom_shpes/containers/primary_header_container.dart';
+import 'package:flutter_ui_app/features/shop/screens/widgets/home_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
+            /// header --
             TPrimaryHeaderContainer(
-              child: Container(),
-            ),
+                child: Column(
+              children: [
+                ///Appbar
+                THomeAppBar(),
+
+                /// Searchbar
+                /// Categories
+                ///
+              ],
+            )),
           ],
         ),
       ),
