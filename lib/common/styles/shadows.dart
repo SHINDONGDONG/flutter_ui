d13a0508d59645154a2f2fb825a295c9075df7f0
신민철 <homerjay@naver.com>
2023-11-17 14:58:12 +0900

    ADD Product Vertical Card,Rouned Card, Positioned

---
+++ lib/common/styles/shadows.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_app/utils/constants/colors.dart';
+
+class TSahdowStyle {
+  static final verticalProductShadow = BoxShadow(
+    color: TColors.darkGrey.withOpacity(0.1),
+    blurRadius: 50,
+    spreadRadius: 7,
+    offset: const Offset(0, 2),
+  );
+
+  static final horizontalProductShadow = BoxShadow(
+    color: TColors.darkGrey.withOpacity(0.1),
+    blurRadius: 50,
+    spreadRadius: 7,
+    offset: const Offset(0, 2),
+  );
+}
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_app/common/widgets/custom_shpes/containers/primary_header_container.dart';
 import 'package:flutter_ui_app/common/widgets/custom_shpes/containers/search_container.dart';
+import 'package:flutter_ui_app/common/widgets/layouts/grid_layout.dart';
+import 'package:flutter_ui_app/common/widgets/products/product_cards/product_card_vertical.dart';
 import 'package:flutter_ui_app/common/widgets/texts/section_heading.dart';
 import 'package:flutter_ui_app/features/shop/screens/widgets/home_appbar.dart';
 import 'package:flutter_ui_app/features/shop/screens/widgets/home_categories.dart';
@@ -14,12 +16,12 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             /// header --
-            TPrimaryHeaderContainer(
+            const TPrimaryHeaderContainer(
               child: Column(
                 children: [
                   ///Appbar
@@ -56,12 +58,24 @@ class HomeScreen extends StatelessWidget {
 
             /// body
             Padding(
-              padding: EdgeInsets.all(TSizes.defaultSpace),
-              child: TPromoSlider(
-                banners: [
-                  TImages.promoBanner1,
-                  TImages.promoBanner2,
-                  TImages.promoBanner3
+              padding: const EdgeInsets.all(TSizes.defaultSpace),
+              child: Column(
+                children: [
+                  const TPromoSlider(
+                    banners: [
+                      TImages.promoBanner1,
+                      TImages.promoBanner2,
+                      TImages.promoBanner3
+                    ],
+                  ),
+
+                  /// --Popular Products
+                  TGridLayout(
+                    itemCount: 4,
+                    itemBuilder: (BuildContext, int) {
+                      return const TProductCardVertical();
+                    },
+                  ),
                 ],
               ),
             ),
